37baad8aea64846e5aea78255e3ec9fbdc47b6f9
Owen Kieffer-Jones <okj579@users.noreply.github.com>
2023-08-30 18:25:30 +0200

    Add video URLs to ARD Mediathek

--- bridges/ARDMediathekBridge.php
+++ bridges/ARDMediathekBridge.php
@@ -14,19 +14,24 @@ class ARDMediathekBridge extends BridgeAbstract
      * The JSON-field pagination holds more information on that
      * @const PAGESIZE number of requested items
      */
-    const PAGESIZE = 29;
+    const PAGESIZE = 3;
     /*
      * The URL Prefix of the (Webapp-)API
      * @const APIENDPOINT https-URL of the used endpoint
      */
-    const APIENDPOINT = 'https://api.ardmediathek.de/page-gateway/widgets/ard/asset/';
+    const APIENDPOINT = 'https://api.ardmediathek.de/page-gateway/widgets/ard/asset/{id}/?pageSize={pageSize}';
+    /*
+     * The URL Prefix of the (Webapp-)API
+     * @const APIENDPOINTMEDIA https-URL of the used endpoint
+     */
+    const APIENDPOINTMEDIA = 'https://api.ardmediathek.de/page-gateway/mediacollection/{id}?devicetype=pc&embedded=true';
     /*
      * The URL prefix of the video link
      * URLs from the webapp include a slug containing titles of show, episode, and tv station.
      * It seems to work without that.
-     * @const VIDEOLINKPREFIX https-URL prefix of video links
+     * @const VIDEOLINK https-URL of video links
      */
-    const VIDEOLINKPREFIX = 'https://www.ardmediathek.de/video/';
+    const VIDEOLINK = 'https://www.ardmediathek.de/video/{id}';
     /*
      * The requested width of the preview image
      * 432 has been observed on the wild
@@ -72,19 +77,31 @@ class ARDMediathekBridge extends BridgeAbstract
             }
         }
 
-        $url = self::APIENDPOINT . $showID . '/?pageSize=' . self::PAGESIZE;
+        $url = strtr(self::APIENDPOINT, ['{id}' => $showID, '{pageSize}' => self::PAGESIZE]);
         $rawJSON = getContents($url);
         $processedJSON = json_decode($rawJSON);
 
         foreach ($processedJSON->teasers as $video) {
             $item = [];
             // there is also ->links->self->id, ->links->self->urlId, ->links->target->id, ->links->target->urlId
-            $item['uri'] = self::VIDEOLINKPREFIX . $video->id . '/';
+            $item['uri'] = strtr(self::VIDEOLINK, ['{id}' => $video->id]);
             // there is also ->mediumTitle and ->shortTitle
             $item['title'] = $video->longTitle;
-            // in the test, aspect16x9 was the only child of images, not sure whether that is always true
+
+            $mediaUrl = strtr(self::APIENDPOINTMEDIA, ['{id}' => $video->id]);
+            $mediaData = json_decode(getContents($mediaUrl));
+
+            $selectedMedia = null;
+            foreach ($mediaData->_mediaArray[0]->_mediaStreamArray as $candidate) {
+                if (!$selectedMedia || ($candidate->_quality !== 'auto' && $candidate->_quality > $selectedMedia->_quality)) {
+                    $selectedMedia = $candidate;
+                }
+            }
+
             $item['enclosures'] = [
-                str_replace(self::IMAGEWIDTHPLACEHOLDER, self::IMAGEWIDTH, $video->images->aspect16x9->src)
+                // in the test, aspect16x9 was the only child of images, not sure whether that is always true
+                str_replace(self::IMAGEWIDTHPLACEHOLDER, self::IMAGEWIDTH, $video->images->aspect16x9->src),
+                $selectedMedia->_stream
             ];
             $item['content'] = '<img src="' . $item['enclosures'][0] . '" /><p>';
             $item['timestamp'] = $video->broadcastedOn;
